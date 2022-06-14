c359b2e4060404e0769f95c99d58eaebdef4071b
sonamm19 <mohitesonam@ymail.com>
2022-06-14 13:57:08 +1200

    Initial commit

---
+++ AsyncProgramming/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace AsyncProgramming
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.syncMethod = new System.Windows.Forms.Button();
+			this.asyncMethod = new System.Windows.Forms.Button();
+			this.parallelMethod = new System.Windows.Forms.Button();
+			this.txtSyncTime = new System.Windows.Forms.TextBox();
+			this.txtAsyncTime = new System.Windows.Forms.TextBox();
+			this.txtParellelTime = new System.Windows.Forms.TextBox();
+			this.txtTaskId = new System.Windows.Forms.TextBox();
+			this.richTxtInfo = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// syncMethod
+			// 
+			this.syncMethod.Location = new System.Drawing.Point(84, 12);
+			this.syncMethod.Name = "syncMethod";
+			this.syncMethod.Size = new System.Drawing.Size(157, 23);
+			this.syncMethod.TabIndex = 0;
+			this.syncMethod.Text = "Synchronized Method";
+			this.syncMethod.UseVisualStyleBackColor = true;
+			this.syncMethod.Click += new System.EventHandler(this.syncMethod_Click);
+			// 
+			// asyncMethod
+			// 
+			this.asyncMethod.Location = new System.Drawing.Point(84, 59);
+			this.asyncMethod.Name = "asyncMethod";
+			this.asyncMethod.Size = new System.Drawing.Size(157, 23);
+			this.asyncMethod.TabIndex = 1;
+			this.asyncMethod.Text = "Asynchronized Method";
+			this.asyncMethod.UseVisualStyleBackColor = true;
+			this.asyncMethod.Click += new System.EventHandler(this.asyncMethod_Click);
+			// 
+			// parallelMethod
+			// 
+			this.parallelMethod.Location = new System.Drawing.Point(84, 110);
+			this.parallelMethod.Name = "parallelMethod";
+			this.parallelMethod.Size = new System.Drawing.Size(187, 23);
+			this.parallelMethod.TabIndex = 2;
+			this.parallelMethod.Text = "Parallel Asynchronized Method";
+			this.parallelMethod.UseVisualStyleBackColor = true;
+			this.parallelMethod.Click += new System.EventHandler(this.parallelMethod_Click);
+			// 
+			// txtSyncTime
+			// 
+			this.txtSyncTime.Location = new System.Drawing.Point(350, 15);
+			this.txtSyncTime.Name = "txtSyncTime";
+			this.txtSyncTime.Size = new System.Drawing.Size(100, 20);
+			this.txtSyncTime.TabIndex = 3;
+			// 
+			// txtAsyncTime
+			// 
+			this.txtAsyncTime.Location = new System.Drawing.Point(350, 62);
+			this.txtAsyncTime.Name = "txtAsyncTime";
+			this.txtAsyncTime.Size = new System.Drawing.Size(100, 20);
+			this.txtAsyncTime.TabIndex = 4;
+			// 
+			// txtParellelTime
+			// 
+			this.txtParellelTime.Location = new System.Drawing.Point(350, 113);
+			this.txtParellelTime.Name = "txtParellelTime";
+			this.txtParellelTime.Size = new System.Drawing.Size(100, 20);
+			this.txtParellelTime.TabIndex = 5;
+			// 
+			// txtTaskId
+			// 
+			this.txtTaskId.Location = new System.Drawing.Point(594, 150);
+			this.txtTaskId.Name = "txtTaskId";
+			this.txtTaskId.ReadOnly = true;
+			this.txtTaskId.Size = new System.Drawing.Size(207, 20);
+			this.txtTaskId.TabIndex = 6;
+			// 
+			// richTxtInfo
+			// 
+			this.richTxtInfo.Location = new System.Drawing.Point(-3, 176);
+			this.richTxtInfo.Name = "richTxtInfo";
+			this.richTxtInfo.Size = new System.Drawing.Size(804, 277);
+			this.richTxtInfo.TabIndex = 7;
+			this.richTxtInfo.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Enabled = false;
+			this.label1.Location = new System.Drawing.Point(536, 150);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(48, 13);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Task No";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.richTxtInfo);
+			this.Controls.Add(this.txtTaskId);
+			this.Controls.Add(this.txtParellelTime);
+			this.Controls.Add(this.txtAsyncTime);
+			this.Controls.Add(this.txtSyncTime);
+			this.Controls.Add(this.parallelMethod);
+			this.Controls.Add(this.asyncMethod);
+			this.Controls.Add(this.syncMethod);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button syncMethod;
+		private System.Windows.Forms.Button asyncMethod;
+		private System.Windows.Forms.Button parallelMethod;
+		private System.Windows.Forms.TextBox txtSyncTime;
+		private System.Windows.Forms.TextBox txtAsyncTime;
+		private System.Windows.Forms.TextBox txtParellelTime;
+		private System.Windows.Forms.TextBox txtTaskId;
+		private System.Windows.Forms.RichTextBox richTxtInfo;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ AsyncProgramming/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncProgramming
+{
+	public partial class Form1 : Form
+	{
+		delegate void TaskTextHandler(string text);
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private List<string> Prepdata()
+		{
+			List<string> urlList = new List<string>();
+			urlList.Add("https://stackoverflow.com/questions/1048199/easiest-way-to-read-from-a-url-into-a-string-in-net/1048204");
+			urlList.Add("https://nz.news.yahoo.com/");
+			urlList.Add("https://stackoverflow.com/questions/298745/how-do-i-send-a-cross-domain-post-request-via-javascript?rq=1");
+			urlList.Add("https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript?rq=1");
+			urlList.Add("https://stackoverflow.com/questions/55686928/using-stopwatch-in-c-sharp");
+
+			return urlList;
+		}
+
+		private void SetTaskText(string taskId)
+		{
+			string separator = "";
+			if (!string.IsNullOrEmpty(txtTaskId.Text))
+				separator = "_";
+			txtTaskId.Text += separator + taskId ;
+		}
+		DataModel DownloadUrl(string url)
+		{
+			try
+			{
+				string s;
+				DataModel dm = new DataModel();
+				dm.urlName = url;
+				if (this.txtTaskId.InvokeRequired)
+				{
+					TaskTextHandler d = SetTaskText;
+					Invoke(d, Thread.CurrentThread.ManagedThreadId.ToString());
+				}
+				else
+				{
+					SetTaskText(Thread.CurrentThread.ManagedThreadId.ToString());
+				}
+				Console.WriteLine("**" + Thread.CurrentThread.ManagedThreadId);
+				using (WebClient client = new WebClient())
+				{
+					s = client.DownloadString(url);
+				}
+
+				dm.count = s.Length;
+				return dm;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+		}
+		void PrintInfo(DataModel dm)
+		{
+			richTxtInfo.Text += "\ncount " + dm.count + " " + dm.urlName;
+		}
+
+		/// <summary>
+		/// Download contents from all website in a sequential manner. One by one
+		/// Waits for results and UI gets stuck until all work is done.
+		/// Result shows on the screen all at once
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void syncMethod_Click(object sender, EventArgs e)
+		{
+			txtTaskId.Text = "";
+			richTxtInfo.Text = "";
+			List<string> urls = Prepdata();
+			var timer = new Stopwatch();
+			timer.Start();
+			DownloadContents(urls);
+			timer.Stop();
+			txtSyncTime.Text = timer.ElapsedMilliseconds.ToString();
+		}
+
+		/// <summary>
+		/// Download content asynchronously (Not all together)
+		/// Still waits for each other but UI allows user to go on with their work. Even while we are waiting for this.
+		/// Result shows on the screen on by one
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private async void asyncMethod_Click(object sender, EventArgs e)
+		{
+			txtTaskId.Text = "";
+			richTxtInfo.Text = "";
+			List<string> urls = Prepdata();
+			var timer = new Stopwatch();
+			timer.Start();
+			await DownloadContentsAsync(urls);
+			timer.Stop();
+			txtAsyncTime.Text = timer.ElapsedMilliseconds.ToString();
+		}
+
+		/// <summary>
+		/// Download content asynchronously (all together)
+		/// Still waits for result for each other but UI alows user to go on with their work. Even while we are waiting for this.
+		/// Result shows on the screen all at once
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private async void parallelMethod_Click(object sender, EventArgs e)
+		{
+			txtTaskId.Text = "";
+			richTxtInfo.Text = "";
+			List<string> urls = Prepdata();
+			var timer = new Stopwatch();
+			timer.Start();
+			await DownloadContentsParallelAsync(urls);
+			timer.Stop();
+			txtParellelTime.Text = timer.ElapsedMilliseconds.ToString();
+		}
+
+		#region Sync
+		void DownloadContents(List<string> urls)
+		{
+			ReadUrlLength(urls);
+		}
+
+
+		void ReadUrlLength(List<string> urls)
+		{
+			foreach (string url in urls)
+			{
+				DataModel dm = DownloadUrl(url);
+				PrintInfo(dm);
+			}
+		}
+		#endregion
+
+		#region Async
+		async Task ReadUrlLengthAsync(List<string> urls)
+		{
+			List<Task<DataModel>> tasks = new List<Task<DataModel>>();
+
+			foreach (string url in urls)
+			{
+				DataModel dm = await Task.Run(()=> DownloadUrl(url));
+				PrintInfo(dm);
+			}
+		}
+		async Task DownloadContentsAsync(List<string> urls)
+		{
+			await ReadUrlLengthAsync(urls);
+		}
+
+		#endregion
+
+		#region ParallelAsync
+		async Task ReadUrlLengthParallelAsync(List<string> urls)
+		{
+			List<Task<DataModel>> tasks = new List<Task<DataModel>>();
+
+			foreach (string url in urls)
+			{
+				tasks.Add(Task.Run(() => DownloadUrl(url)));
+			}
+
+			var result = await Task.WhenAll(tasks);
+			foreach (var item in result)
+			{
+				PrintInfo(item);
+			}
+
+		}
+		async Task DownloadContentsParallelAsync(List<string> urls)
+		{
+			await ReadUrlLengthParallelAsync(urls);
+		}
+
+		#endregion
+
+	}
+
+	public class DataModel
+	{
+		public int count;
+		public string urlName;
+	}
+}
